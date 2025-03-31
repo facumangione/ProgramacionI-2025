8fb751ef2212863cad7f5661849acb556c2332db
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2025-03-31 17:35:47 -0300

    actualizacion nobre de archivo usuario

--- backend/main/resources/__init__.py
+++ backend/main/resources/__init__.py
@@ -1,4 +1,4 @@
-from .animal import Usuario as UsuarioResource
-from .animal import Usuarios as UsuariosResource
+from .usuario import Usuario as UsuarioResource
+from .usuario import Usuarios as UsuariosResource
 from .pedido import Pedido as PedidoResource
 from .pedido import Pedidos as PedidosResource
